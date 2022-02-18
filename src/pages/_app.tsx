c322161ed1001921921002a5b7fe211c9d058f16
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2022-02-18 08:48:00 +0100

    refactor detectMobile feature to work for entire app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,41 @@ import "@/assets/styles/third-party/slick/slick-theme.css";
 import "@/assets/styles/third-party/slick/slick.min.css";
 import "@/assets/styles/globals.css";
 import "tailwindcss/tailwind.css";
+import { useEffect, useState } from "react";
+import { detectMobile } from "@/utils/detectMobile";
 
 const MyApp = ({ Component, pageProps }) => {
+   const [isMobile, setIsMobile] = useState(false);
+   const [isLoading, setIsLoading] = useState(true);
+
+   useEffect(() => {
+      setIsLoading(false);
+      if (detectMobile() && document.documentElement.clientWidth <= 768) {
+         setIsMobile(true);
+      }
+   }, []);
+
+   if (isLoading) {
+      return (
+         <div className="flex h-screen items-center justify-center">
+            <div className="text-center text-primary-500">
+               <p className="text-xl">Loading.....</p>
+            </div>
+         </div>
+      );
+   }
+
+   if (isMobile) {
+      return (
+         <div className="flex h-screen items-center justify-center">
+            <div className="text-center text-primary-500">
+               <p className="text-xl">App only renders well on bigger screens.</p>
+               <h1 className="mt-2 text-3xl font-bold">Please check on PC</h1>
+            </div>
+         </div>
+      );
+   }
+
    return <Component {...pageProps} />;
 };
 
